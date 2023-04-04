54a7801fedbc41d610eba76f9cac5d917c0d22ac
Mishaal <mishaal.20210822@iit.ac.lk>
2023-04-04 14:32:10 +0530

    finished main functions for all data methods

--- data/id_data.py
+++ data/id_data.py
@@ -13,29 +13,58 @@ def get_playlist():
     url = input("Enter Playlist URL > \n")
     playlist_id = url.split('/')[-1].split('?')[0]
     print(playlist_id)
-    # playlist_id = "5oqCWuHcpkcf9be6WqL3n8"
     return playlist_id
 
 
 # gets all the tracks in the playlist
-playlist_id = get_playlist()
-results = sp.playlist_items(playlist_id)
-tracks = results['items']
-while results['next']:
-    results = sp.next(results)
-    tracks.extend(results['items'])
+def get_tracks(playlist_id):
+    results = sp.playlist_items(playlist_id)
+    tracks = results['items']
+    while results['next']:
+        results = sp.next(results)
+        tracks.extend(results['items'])
+    return tracks
+
 
 # gets all the track IDs
-track_ids = []
-for track in tracks:
-    if track['track']:
-        track_id = track['track']['id']
-        track_ids.append(track_id)
-print(track_ids)
-print(len(track_ids))
+def get_track_ids(tracks):
+    track_ids = []
+    for track in tracks:
+        if track['track']:
+            track_id = track['track']['id']
+            track_ids.append(track_id)
 
-audio_features = sp.audio_features(track_ids)
-print(audio_features)
+    print(track_ids)
+    print(len(track_ids))
+    return track_ids
 
-df = pd.DataFrame.from_records(audio_features)
-print(df.head())
+
+# get audio features of the tracks
+def get_audio_features(track_ids):
+    audio_features = sp.audio_features(track_ids)
+    print(audio_features)
+    return audio_features
+
+
+# create new df using audio features
+def new_df(audio_features, type):
+    df = pd.DataFrame.from_records(audio_features)
+    if type == 0:  # good songs
+        df.to_csv('data/good_songs.csv')
+    else:  # bad songs
+        df.to_csv('data/bad_songs.csv')
+
+
+# add to existing list (good/bad)
+def existing_df(audio_features, type):
+    if type == 0:
+        df = pd.read_csv('data/good_songs.csv')
+    else:
+        df = pd.read_csv('data/bad_songs.csv')
+
+    df = df.append(audio_features, ignore_index=True)
+
+    if type == 0:  # good songs
+        df.to_csv('data/good_songs.csv')
+    else:  # bad songs
+        df.to_csv('data/bad_songs.csv')
